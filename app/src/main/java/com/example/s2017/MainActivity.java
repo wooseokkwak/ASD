02c103bc3b5c14f2a5462912779868ebedef346f
wooseokkwak <kws980407@gmail.com>
2019-03-20 15:17:07 +0900

    Initial Commit

--- app/src/main/java/com/example/s2017/MainActivity.java
+++ app/src/main/java/com/example/s2017/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
